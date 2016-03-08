ad97b7635ceb010a8fafadac30a7bab751c1ab03
David West <thedavidwest@gmail.com>
2016-03-08 11:34:17 -0600

    Edited seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(user_id: 1, email: 'thedavidwest@gmail.com', password: 'rgbyktest')
+User.create(email: 'thedavidwest@gmail.com', password: 'rgbyktest')
 Palette.create(user_id: 1, title: 'Twitter', color: '#1da1f2', color2: '#66757f', color3: '#292f33', color4: '#f5f8fa')
 Palette.create(user_id: 1, title: 'Hulu', color: '#66AA33', color2: '#FFFFFF', color3: '#E2E2E2', color4: '#444444')
 Palette.create(user_id: 1, title: 'Netflix', color: '#1F1F1F', color2: '#E50914', color3: '#999999', color4: '#FFFFFF')
